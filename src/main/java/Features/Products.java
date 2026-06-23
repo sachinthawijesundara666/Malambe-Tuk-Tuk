77865832a5118ea73a3c1c6d9ce7e63380185029
sachinthawijesundara666 <sachintha.20250307@iit.ac.lk>
2026-06-23 10:36:44 +0530

    Second Commit

--- src/main/java/Features/Products.java
+++ src/main/java/Features/Products.java
@@ -1,7 +1,26 @@
 package Features;
 
 public class Products {
-    public static void main(String[] args){
-        System.out.println("Features.Products Page!");
+    String code;
+    String name;
+    String brand;
+    Double price;
+    int quantity;
+    String detail;
+    String date;
+    String picture;
+
+    public Products(String code, String name, String brand, Double price, int quantity, String detail, String date, String picture){
+        this.code = code;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.quantity = quantity;
+        this.detail = detail;
+        this.date = date;
+        this.picture = picture;
     }
+
+
+
 }
