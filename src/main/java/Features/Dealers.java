64edacc5235c096212e9cf021a654a5ac431225b
sachinthawijesundara666 <sachintha.20250307@iit.ac.lk>
2026-06-24 10:45:12 +0530

    Second Commit

--- src/main/java/Features/Dealers.java
+++ src/main/java/Features/Dealers.java
@@ -1,4 +1,15 @@
 package Features;
 
 public class Dealers {
+    String code;
+    String name;
+    String contact;
+    String location;
+
+    public Dealers(String code, String name, String contact, String location){
+        this.code = code;
+        this.name = name;
+        this.contact = contact;
+        this.location = location;
+    }
 }
