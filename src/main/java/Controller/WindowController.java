5eb966eeccad4616a5c8acfbba78ddba4fb2af90
sachinthawijesundara666 <sachintha.20250307@iit.ac.lk>
2026-07-04 18:37:24 +0530

    Seventh Commit

--- src/main/java/Controller/WindowController.java
+++ src/main/java/Controller/WindowController.java
@@ -1,4 +1,13 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 public class WindowController {
+
+    @FXML
+    private Label help;
+
+    @FXML
+    protected void onHelpClick(){help.setText("Welcome!");
+    }
 }
--- src/main/java/MainProgram/MalambeTukTuk.java
+++ src/main/java/MainProgram/MalambeTukTuk.java
@@ -9,6 +9,11 @@ import java.io.IOException;
 public class MalambeTukTuk extends Application{
     @Override
     public void start(Stage stage) throws IOException{
+        FXMLLoader fxmlloader = new FXMLLoader(MalambeTukTuk.class.getResource("/FXML/MalambeTukTuk.fxml"));
+        Scene scene =  new Scene(fxmlloader.load(), 713, 437);
+        stage.setTitle("Malambe Tuk Tuk Spare Parts Inventory Manager");
+        stage.setScene(scene);
+        stage.show();
 
     }
 
