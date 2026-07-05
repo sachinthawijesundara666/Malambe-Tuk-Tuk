26efc14af97af7dee0f7ff8ac3f845fedfcdb720
sachinthawijesundara666 <sachintha.20250307@iit.ac.lk>
2026-07-06 00:05:21 +0530

    Eightth Commit

--- src/main/java/Features/TextFileManager.java
+++ src/main/java/Features/TextFileManager.java
@@ -1,9 +1,14 @@
 package Features;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class TextFileManager {
+
+    //Appending to a text file
     public boolean append(String textfile, String dataObjString){
         try {
          FileWriter writer = new FileWriter("/Data/"+textfile, true);
@@ -16,7 +21,24 @@ public class TextFileManager {
         }
     }
 
+    //Reading a Text File
     public void read(String textfile){
+        try {
+            int count = 0;
+            File file = new File("/Data/"+textfile);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()){
+                scanner.nextLine();
+                count++;
+            }
+
+            for (int i=0 ; i<count ; i++){
+                String[] item = new String[4];
+            }
+        } catch (FileNotFoundException e) {
+            return;
+        }
     }
 
 }
